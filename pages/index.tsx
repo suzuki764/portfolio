320b45ee7f8d41798d6b1409abf6adb76b63aac8
Kosuke Suzuki <k.suzuki764@gmail.com>
2022-11-23 01:36:50 +0900

    Anchorを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import { Canvas } from "@react-three/fiber";
 import { Stars } from "../components/Stars";
+import { Anchor } from "antd";
+
+const { Link } = Anchor;
 
 export default function Home() {
+  const getCurrentAnchor = () => "./";
+
   return (
     <div>
       <Head>
@@ -17,14 +23,19 @@ export default function Home() {
           <Stars />
         </Canvas>
         <div className={styles.title}>
-          <h1>
-            Kosuke Suzuki&#39;s
-            <br />
-            Portfolio Site!
-          </h1>
+          <h1>Kosuke Suzuki&#39;s Portfolio Site</h1>
+        </div>
+        <div style={{ position: "absolute", top: 0, margin: "1rem" }}>
+          <Anchor getCurrentAnchor={getCurrentAnchor}>
+            <Link href="./" title="Top" />
+            <Link href="./aboutme" title="About Me" />
+            <Link href="./products" title="Products">
+              <Link href="./apps" title="Apps" />
+              <Link href="./games" title="Games" />
+            </Link>
+          </Anchor>
         </div>
       </main>
-
       <footer className={styles.footer}>© 2022 Kosuke Suzuki</footer>
     </div>
   );
