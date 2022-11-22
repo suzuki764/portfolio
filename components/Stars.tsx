bb00e2d12d9db0fbf059b3e7786eac83fb0c8dc3
Kosuke Suzuki <k.suzuki764@gmail.com>
2022-11-23 00:05:22 +0900

    背景を作成

---
+++ components/Stars.tsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Points, PointMaterial } from "@react-three/drei";
+import { random } from "maath";
+import THREE from "three";
+
+export function Stars(props: any) {
+  const ref = useRef<THREE.Points>(null!);
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.5 })
+  );
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled={false}
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#5383c3"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
